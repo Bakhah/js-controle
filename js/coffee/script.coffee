eff383c9582d263fac08565c6b67e3fbe14621a5
bakhah <franck.hourdin@pictotravel.fr>
2017-03-22 18:26:03 +0100

    End

--- js/coffee/script.coffee
+++ js/coffee/script.coffee
@@ -1,34 +1,42 @@
 listen_submit = ->
   button = jQuery('input.btn')
-  jQuery('form').on 'submit', (e) =>
+  jQuery('form').on 'submit', (e) ->
     e.preventDefault()
     clear_warnings()
     search_field = jQuery('[name="search"]')
-    console.log search_field
     if search_field.val()
       empty_results_view()
       show_loading()
       $.getJSON 'proxy.php', {search: search_field.val()}, (json) ->
-        display_results(json)
         hide_loading()
+        display_results(json)
     else
       warn_user_search_field_is_empty()
 
 display_results = (json) ->
   search_results = json.query.search
   result_items_view = jQuery(".results")
-  console.log search_results
-  if search_results.length != 0
+  if search_results && search_results.length != 0
     for result in search_results
       result_items_view.append('<li class="list-group-item clearfix">
-                                <a href="#">
+                                <a class="result_item" href="#" data-title="' + result.title + '">
                                   <h3 class="list-group-item-heading">' + result.title + '</h3>
                                   <p class="list-group-item-text">' + result.snippet + '</p>
                                 </a>
                               </li>')
+    reset_item_listeners()
   else
     result_items_view.empty()
     result_items_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>')
+
+display_results_description = (json) ->
+  results = json.query.pages
+  item_title_view = jQuery(".detailContainer h3")
+  item_content_view = jQuery(".detailContainer p")
+
+  $.each results, (value) ->
+    item_title_view.html(results[value].title)
+    item_content_view.html(results[value].revisions["0"]["*"])
 
 empty_results_view = ->
   jQuery(".results").empty()
@@ -57,6 +65,13 @@ clear_warnings = ->
   search_field = jQuery('[name="search"]')
   search_field.css('border-color', '#ccc')
   jQuery('#warning').remove()
+
+reset_item_listeners = ->
+  jQuery('.result_item').click ->
+    item = jQuery(this)
+    $.getJSON 'proxy.php', {title: item.data('title')}, (json) ->
+      hide_loading()
+      display_results_description(json)
 
 jQuery(document).ready(ready_document);
 
--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,47 +1,53 @@
-var clear_warnings, display_results, empty_results_view, hide_loading, listen_submit, ready_document, show_loading, warn_user_search_field_is_empty;
+var clear_warnings, display_results, display_results_description, empty_results_view, hide_loading, listen_submit, ready_document, reset_item_listeners, show_loading, warn_user_search_field_is_empty;
 
 listen_submit = function() {
   var button;
   button = jQuery('input.btn');
-  return jQuery('form').on('submit', (function(_this) {
-    return function(e) {
-      var search_field;
-      e.preventDefault();
-      clear_warnings();
-      search_field = jQuery('[name="search"]');
-      console.log(search_field);
-      if (search_field.val()) {
-        empty_results_view();
-        show_loading();
-        return $.getJSON('proxy.php', {
-          search: search_field.val()
-        }, function(json) {
-          display_results(json);
-          return hide_loading();
-        });
-      } else {
-        return warn_user_search_field_is_empty();
-      }
-    };
-  })(this));
+  return jQuery('form').on('submit', function(e) {
+    var search_field;
+    e.preventDefault();
+    clear_warnings();
+    search_field = jQuery('[name="search"]');
+    if (search_field.val()) {
+      empty_results_view();
+      show_loading();
+      return $.getJSON('proxy.php', {
+        search: search_field.val()
+      }, function(json) {
+        hide_loading();
+        return display_results(json);
+      });
+    } else {
+      return warn_user_search_field_is_empty();
+    }
+  });
 };
 
 display_results = function(json) {
-  var i, len, result, result_items_view, results, search_results;
+  var i, len, result, result_items_view, search_results;
   search_results = json.query.search;
   result_items_view = jQuery(".results");
-  console.log(search_results);
-  if (search_results.length !== 0) {
-    results = [];
+  if (search_results && search_results.length !== 0) {
     for (i = 0, len = search_results.length; i < len; i++) {
       result = search_results[i];
-      results.push(result_items_view.append('<li class="list-group-item clearfix"> <a href="#"> <h3 class="list-group-item-heading">' + result.title + '</h3> <p class="list-group-item-text">' + result.snippet + '</p> </a> </li>'));
+      result_items_view.append('<li class="list-group-item clearfix"> <a class="result_item" href="#" data-title="' + result.title + '"> <h3 class="list-group-item-heading">' + result.title + '</h3> <p class="list-group-item-text">' + result.snippet + '</p> </a> </li>');
     }
-    return results;
+    return reset_item_listeners();
   } else {
     result_items_view.empty();
     return result_items_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>');
   }
+};
+
+display_results_description = function(json) {
+  var item_content_view, item_title_view, results;
+  results = json.query.pages;
+  item_title_view = jQuery(".detailContainer h3");
+  item_content_view = jQuery(".detailContainer p");
+  return $.each(results, function(value) {
+    item_title_view.html(results[value].title);
+    return item_content_view.html(results[value].revisions["0"]["*"]);
+  });
 };
 
 empty_results_view = function() {
@@ -73,6 +79,19 @@ clear_warnings = function() {
   search_field = jQuery('[name="search"]');
   search_field.css('border-color', '#ccc');
   return jQuery('#warning').remove();
+};
+
+reset_item_listeners = function() {
+  return jQuery('.result_item').click(function() {
+    var item;
+    item = jQuery(this);
+    return $.getJSON('proxy.php', {
+      title: item.data('title')
+    }, function(json) {
+      hide_loading();
+      return display_results_description(json);
+    });
+  });
 };
 
 jQuery(document).ready(ready_document);
