e0651b04404e55ad5ba15c9e583df0ef0cf6ad9d
bakhah <franck.hourdin@pictotravel.fr>
2017-03-23 12:32:58 +0100

    Refactor, ok to send

--- js/coffee/script.coffee
+++ js/coffee/script.coffee
@@ -1,76 +1,83 @@
+search_field = undefined
+items_search_view = undefined
+item_title_view = undefined
+item_content_view = undefined
+main_form = undefined
+main_title = undefined
+
+initialize_globals = ->
+  search_field = jQuery('[name="search"]')
+  items_search_view = jQuery(".results")
+  item_title_view = jQuery(".detailContainer h3")
+  item_content_view = jQuery(".detailContainer p")
+  main_form = jQuery('form')
+  main_title = jQuery('form h1')
+
 listen_submit = ->
-  button = jQuery('input.btn')
-  jQuery('form').on 'submit', (e) ->
+  main_form.on 'submit', (e) ->
     e.preventDefault()
     clear_warnings()
-    search_field = jQuery('[name="search"]')
     if search_field.val()
       empty_results_view()
       show_loading()
       $.getJSON 'proxy.php', {search: search_field.val()}, (json) ->
         hide_loading()
-        display_results(json)
+        display_search_results(json)
     else
       warn_user_search_field_is_empty()
 
-display_results = (json) ->
-  search_results = json.query.search
-  result_items_view = jQuery(".results")
-  if search_results && search_results.length != 0
-    for result in search_results
-      result_items_view.append('<li class="list-group-item clearfix">
-                                <a class="result_item" href="#" data-title="' + result.title + '">
-                                  <h3 class="list-group-item-heading">' + result.title + '</h3>
-                                  <p class="list-group-item-text">' + result.snippet + '</p>
+display_search_results = (json) ->
+  json_search_results = json.query.search
+  if json_search_results && json_search_results.length != 0
+    for item in json_search_results
+      items_search_view.append('<li class="list-group-item clearfix">
+                                <a class="result_item" href="#" data-title="' + item.title + '">
+                                  <h3 class="list-group-item-heading">' + item.title + '</h3>
+                                  <p class="list-group-item-text">' + item.snippet + '</p>
                                 </a>
                               </li>')
     reset_item_listeners()
   else
-    result_items_view.empty()
-    result_items_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>')
+    items_search_view.empty()
+    items_search_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>')
 
 display_results_description = (json) ->
-  results = json.parse
-  item_title_view = jQuery(".detailContainer h3")
-  item_content_view = jQuery(".detailContainer p")
+  item_infos = json.parse
 
-  item_title_view.html(results.title)
-  item_content_view.html(results.text["*"])
+  item_title_view.html(item_infos.title)
+  item_content_view.html(item_infos.text["*"])
+
+reset_item_listeners = ->
+  jQuery('.result_item').click ->
+    show_loading()
+    $.getJSON 'proxy.php', {title: jQuery(this).data('title')}, (json) ->
+      hide_loading()
+      display_results_description(json)
+
+
 
 empty_results_view = ->
-  jQuery(".results").empty()
+  items_search_view.empty()
 
 show_loading = ->
-  jQuery('.results').append('<div id="loading">
-                              <center>
-                                <h1>Chargement en cours...</h1>
-                                <img src="assets/animations/spin.svg"></img>
-                              <center>
-                            </div>')
+  hide_loading()
+  main_title.append('<img id="loading" src="assets/animations/spin.svg"></img>')
 
 hide_loading = ->
   jQuery('#loading').remove()
 
-ready_document = ->
-  listen_submit()
-  console.log 'ready_document events bound'
-
 warn_user_search_field_is_empty = ->
-  search_field = jQuery('[name="search"]')
   search_field.css('border-color', 'red')
   jQuery('form').append('<div id="warning"><strong>Le champ est vide</strong> Entrez au moins un caractère</div>')
 
 clear_warnings = ->
-  search_field = jQuery('[name="search"]')
   search_field.css('border-color', '#ccc')
   jQuery('#warning').remove()
 
-reset_item_listeners = ->
-  jQuery('.result_item').click ->
-    item = jQuery(this)
-    $.getJSON 'proxy.php', {title: item.data('title')}, (json) ->
-      hide_loading()
-      display_results_description(json)
+ready_document = ->
+  initialize_globals()
+  listen_submit()
+  console.log 'ready_document events bound'
 
 jQuery(document).ready(ready_document);
 
--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,13 +1,30 @@
-var clear_warnings, display_results, display_results_description, empty_results_view, hide_loading, listen_submit, ready_document, reset_item_listeners, show_loading, warn_user_search_field_is_empty;
+var clear_warnings, display_results_description, display_search_results, empty_results_view, hide_loading, initialize_globals, item_content_view, item_title_view, items_search_view, listen_submit, main_form, main_title, ready_document, reset_item_listeners, search_field, show_loading, warn_user_search_field_is_empty;
+
+search_field = void 0;
+
+items_search_view = void 0;
+
+item_title_view = void 0;
+
+item_content_view = void 0;
+
+main_form = void 0;
+
+main_title = void 0;
+
+initialize_globals = function() {
+  search_field = jQuery('[name="search"]');
+  items_search_view = jQuery(".results");
+  item_title_view = jQuery(".detailContainer h3");
+  item_content_view = jQuery(".detailContainer p");
+  main_form = jQuery('form');
+  return main_title = jQuery('form h1');
+};
 
 listen_submit = function() {
-  var button;
-  button = jQuery('input.btn');
-  return jQuery('form').on('submit', function(e) {
-    var search_field;
+  return main_form.on('submit', function(e) {
     e.preventDefault();
     clear_warnings();
-    search_field = jQuery('[name="search"]');
     if (search_field.val()) {
       empty_results_view();
       show_loading();
@@ -15,7 +32,7 @@ listen_submit = function() {
         search: search_field.val()
       }, function(json) {
         hide_loading();
-        return display_results(json);
+        return display_search_results(json);
       });
     } else {
       return warn_user_search_field_is_empty();
@@ -23,73 +40,67 @@ listen_submit = function() {
   });
 };
 
-display_results = function(json) {
-  var i, len, result, result_items_view, search_results;
-  search_results = json.query.search;
-  result_items_view = jQuery(".results");
-  if (search_results && search_results.length !== 0) {
-    for (i = 0, len = search_results.length; i < len; i++) {
-      result = search_results[i];
-      result_items_view.append('<li class="list-group-item clearfix"> <a class="result_item" href="#" data-title="' + result.title + '"> <h3 class="list-group-item-heading">' + result.title + '</h3> <p class="list-group-item-text">' + result.snippet + '</p> </a> </li>');
+display_search_results = function(json) {
+  var i, item, json_search_results, len;
+  json_search_results = json.query.search;
+  if (json_search_results && json_search_results.length !== 0) {
+    for (i = 0, len = json_search_results.length; i < len; i++) {
+      item = json_search_results[i];
+      items_search_view.append('<li class="list-group-item clearfix"> <a class="result_item" href="#" data-title="' + item.title + '"> <h3 class="list-group-item-heading">' + item.title + '</h3> <p class="list-group-item-text">' + item.snippet + '</p> </a> </li>');
     }
     return reset_item_listeners();
   } else {
-    result_items_view.empty();
-    return result_items_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>');
+    items_search_view.empty();
+    return items_search_view.append('<h3>Aucun résultat</h3><h4>Essayez une autre recherche</h4>');
   }
 };
 
 display_results_description = function(json) {
-  var item_content_view, item_title_view, results;
-  results = json.parse;
-  item_title_view = jQuery(".detailContainer h3");
-  item_content_view = jQuery(".detailContainer p");
-  item_title_view.html(results.title);
-  return item_content_view.html(results.text["*"]);
+  var item_infos;
+  item_infos = json.parse;
+  item_title_view.html(item_infos.title);
+  return item_content_view.html(item_infos.text["*"]);
+};
+
+reset_item_listeners = function() {
+  return jQuery('.result_item').click(function() {
+    show_loading();
+    return $.getJSON('proxy.php', {
+      title: jQuery(this).data('title')
+    }, function(json) {
+      hide_loading();
+      return display_results_description(json);
+    });
+  });
 };
 
 empty_results_view = function() {
-  return jQuery(".results").empty();
+  return items_search_view.empty();
 };
 
 show_loading = function() {
-  return jQuery('.results').append('<div id="loading"> <center> <h1>Chargement en cours...</h1> <img src="assets/animations/spin.svg"></img> <center> </div>');
+  hide_loading();
+  return main_title.append('<img id="loading" src="assets/animations/spin.svg"></img>');
 };
 
 hide_loading = function() {
   return jQuery('#loading').remove();
 };
 
-ready_document = function() {
-  listen_submit();
-  return console.log('ready_document events bound');
-};
-
 warn_user_search_field_is_empty = function() {
-  var search_field;
-  search_field = jQuery('[name="search"]');
   search_field.css('border-color', 'red');
   return jQuery('form').append('<div id="warning"><strong>Le champ est vide</strong> Entrez au moins un caractère</div>');
 };
 
 clear_warnings = function() {
-  var search_field;
-  search_field = jQuery('[name="search"]');
   search_field.css('border-color', '#ccc');
   return jQuery('#warning').remove();
 };
 
-reset_item_listeners = function() {
-  return jQuery('.result_item').click(function() {
-    var item;
-    item = jQuery(this);
-    return $.getJSON('proxy.php', {
-      title: item.data('title')
-    }, function(json) {
-      hide_loading();
-      return display_results_description(json);
-    });
-  });
+ready_document = function() {
+  initialize_globals();
+  listen_submit();
+  return console.log('ready_document events bound');
 };
 
 jQuery(document).ready(ready_document);
